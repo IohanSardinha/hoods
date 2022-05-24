246645eb612dac1b209286892b31530e2ba27bf4
Andre <andrega@ethz.ch>
2022-05-24 16:08:40 +0200

    added logo and checkboxes

--- webapp/frontend/src/components/homepage.js
+++ webapp/frontend/src/components/homepage.js
@@ -5,6 +5,7 @@ export default class Homepage extends Component{
     render(){
         return(
             <div>
+                <img src={`${process.env.PUBLIC_URL}/hoods.png`}></img>
                 <h1>HOODS</h1>
                 <p className="lead">HOusing Open Data Survey (or just a bad Google Maps implementation)</p>
                 <hr />
