71d529b5c1fe1fd27c11032bf5e470a18b1194b7
giacomolmb <giacomo.lombardo@mail.polimi.it>
2022-05-25 13:23:08 +0200

    Rocking the frontend

--- webapp/frontend/src/components/searchaddress.js
+++ webapp/frontend/src/components/searchaddress.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { withRouter } from "react-router";
-import { Autocomplete, GoogleMap, LoadScript } from "@react-google-maps/api";
+import { Autocomplete, GoogleMap, Polygon, LoadScript } from "@react-google-maps/api";
 import { useForm, useFormState } from "react-hook-form";
+import { Dropdown } from 'react-bootstrap';
+
+import polys from '../json/polys.json'
 
 
 const libs = ['places']
 const search_keys = ['geometry.location', 'formatted_address']
-const MAPS_API_KEY = "API KEY HERE"
+const MAPS_API_KEY = 'AIzaSyAes3QFRvAKAh29Bpq0ue0BhLoh5Rbo0tc'
 
 const bounds = {
     south: 41.3451778,
@@ -16,11 +19,36 @@ const bounds = {
     west: 2.0768881
 }
 
+const fillColors = ['#ff0000', '#ff9900', '#ffff00', '#80ff00', '#009900']
+
+function PolyBarrio(paths, fillColor){
+    const options = {
+        fillColor: fillColors[fillColor],
+        strokeOpacity: 1,
+        strokeWeight: 1,
+        clickable: false,
+        draggable: false,
+        editable: false,
+        geodesic: false,
+        zIndex: 1
+    }
+
+    const onLoad = polygon => {
+        console.log("polygon: ", polygon);
+    }
+
+    return(
+        <Polygon
+            onLoad={onLoad}
+            paths={paths}
+            options={options}
+        />
+    )
+}
 
 function SearchAddress() {
 
     const { register, handleSubmit, trigger, formState: { errors } } = useForm({ reValidateMode: 'onChange' });
-
 
     const [address, setAddress] = useState("")
 
@@ -34,12 +62,9 @@ function SearchAddress() {
     //    
     //})
 
-
     const onLoadComplete = (ac) => {
         setAutoComplete(ac)
     }
-
-
 
     const onChangeAddress = (e) => {
         setAddress(e.target.value);
@@ -48,7 +73,6 @@ function SearchAddress() {
     const autoCompleteDone = () => {
         setAddress(autocomplete.getPlace().formatted_address)
     }
-
 
     const validateInBarcelona = async (value) => {
 
@@ -71,12 +95,13 @@ function SearchAddress() {
                 }
             });
     }
+
     return (
         <>
             <LoadScript libraries={libs} googleMapsApiKey={MAPS_API_KEY}>
                 <div>
-                    <form className="row" input='submit' onSubmit={handleSubmit(onSubmit)}>
-                        <div className="col-10 form-group" >
+                    <form className="row mb-3" input='submit' onSubmit={handleSubmit(onSubmit)}>
+                        <div className="col-7 form-group" >
                             <Autocomplete fields={search_keys} onLoad={onLoadComplete} onPlaceChanged={autoCompleteDone}>
                                 <input
                                     {...register('addressField', { validate: validateInBarcelona, required: true })}
@@ -90,28 +115,36 @@ function SearchAddress() {
                             </Autocomplete>
                             <div className="text-danger"><small>{errors.addressField && "Address must be in Barcelona"}</small></div>
                         </div>
-                        <div className="col-2">
-                            <button type='submit' className="btn btn-lg btn-outline-success mb-3">Search</button>
-                        </div>
-                        <div classname='row'>
-                        <p className="lead">Quality of Life Priorites</p>
-                            <div class="form-check form-check-inline">
-                                <input {...register('restCheck')} class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
-                                <label class="form-check-label" for="inlineCheckbox1">Restaurants & Cafés</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input {...register('nightCheck')} class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                                <label class="form-check-label" for="inlineCheckbox2">Bars & Nightlife</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input {...register('playCheck')} class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
-                                <label class="form-check-label" for="inlineCheckbox3">Playgrounds & Parks</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input {...register('seeCheck')} class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
-                                <label class="form-check-label" for="inlineCheckbox3">Closeness to Sea</label>
-                            </div>
+                        <div className="col">
+                            <Dropdown>
+                                <Dropdown.Toggle className="w-100" variant="success" id="dropdown-basic">
+                                    Priorities
+                                </Dropdown.Toggle>
 
+                                <Dropdown.Menu>
+                                    <div className="px-2">
+                                        <div className="form-check form-check-inline">
+                                            <input {...register('restCheck')} className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
+                                            <label className="form-check-label" htmlFor="inlineCheckbox1">Restaurants & Cafés</label>
+                                        </div>
+                                        <div className="form-check form-check-inline">
+                                            <input {...register('nightCheck')} className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
+                                            <label className="form-check-label" htmlFor="inlineCheckbox2">Bars & Nightlife</label>
+                                        </div>
+                                        <div className="form-check form-check-inline">
+                                            <input {...register('playCheck')} className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
+                                            <label className="form-check-label" htmlFor="inlineCheckbox3">Playgrounds & Parks</label>
+                                        </div>
+                                        <div className="form-check form-check-inline">
+                                            <input {...register('seeCheck')} className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
+                                            <label className="form-check-label" htmlFor="inlineCheckbox3">Closeness to Sea</label>
+                                        </div>
+                                    </div>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
+                        <div className="col-2">
+                            <button type='submit' className="btn btn-block btn-outline-success w-100">Search</button>
                         </div>
                     </form>
                 </div>
@@ -119,11 +152,15 @@ function SearchAddress() {
                     id="circle-example"
                     mapContainerStyle={{
                         height: "400px",
-                        width: "800px"
+                        width: "100%"
                     }}
                     zoom={15}
                     center={current_center}
-                />
+                >
+                    {polys.map((poly) => {
+                        return PolyBarrio(poly.points, Math.floor(Math.random() * 5))
+                    })}
+                </GoogleMap>
             </LoadScript>
         </>
     );
