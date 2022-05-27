1065d599285031920fabea2dcbcfe80f6d26cbfe
Leonardo Menti <leonardo.menti@studenti.unimi.it>
2022-05-27 14:48:22 +0200

    putting news on dynamo

--- scrape/scrape_news.py
+++ scrape/scrape_news.py
@@ -1,5 +1,19 @@
+import boto3
 import scrapy
 from scrapy.crawler import CrawlerProcess
+
+ids = {
+    "Ciutat Vella" : 1,
+    "L'Eixample" : 2,
+    "Sants-Montjuïc" : 3,
+    "Les Corts" : 4,
+    "Sarrià-Sant Gervasi" : 5,
+    "Gràcia" : 6,
+    "Horta - Guinardó" : 7,
+    "Nou Barris" : 8,
+    "Sant Andreu" : 9,
+    "Sant Martí" : 10
+}
 
 class MySpider(scrapy.Spider):
 
@@ -14,10 +28,37 @@ class MySpider(scrapy.Spider):
             yield response.follow(district_url, callback=self.parse_district)
 
     def parse_district(self, response):
-        print('\nDistrict: ', response.css('h1::text').getall()[2])
-        for title in response.css('div.media-content h3 a::text').getall():
-            print(title)
 
+        district_name = response.css('h1::text').getall()[2]
+
+        if district_name in ids:  
+            
+            news = []
+            for title in response.css('div.media-content'):
+                news_title = title.css('h3 a::text').get()
+                date = title.css('p.date-new::text').get()
+                relative_url = title.css('div.media-content h3 a::attr(href)').get()
+
+                single_news = {
+                    "title" : news_title,
+                    "date" : date,
+                    "url" : 'https://ajuntament.barcelona.cat' + relative_url
+                }
+
+                news.append(single_news)
+
+            item = {
+                "id" : ids[district_name],
+                "district_name" : district_name,
+                "news" : news
+            }
+
+            table.put_item(Item=item)
+        
+
+session=boto3.Session(aws_access_key_id='AKIA56RSXOYZPEUFJTR5',aws_secret_access_key='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', region_name='eu-west-1')
+client_dynamo=session.resource('dynamodb')
+table=client_dynamo.Table('scraped_news')
 
 c = CrawlerProcess()
 c.crawl(MySpider)
